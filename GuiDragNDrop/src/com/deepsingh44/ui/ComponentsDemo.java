d50191f3e9a3bd533bbd1d92f177d85ce776067e
deepsingh44 <deepsingh44@gmail.com>
2021-01-31 17:04:15 +0530

    GUI Done Project Start.

--- GuiDragNDrop/src/com/deepsingh44/ui/ComponentsDemo.java
+++ GuiDragNDrop/src/com/deepsingh44/ui/ComponentsDemo.java
@@ -10,8 +10,19 @@ import javax.swing.JComboBox;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.ItemListener;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.awt.event.ItemEvent;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
 
 public class ComponentsDemo extends JFrame {
 
@@ -73,6 +84,58 @@ public class ComponentsDemo extends JFrame {
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(rdbtnMale);
 		bg.add(rdbtnFemale);
+		
+		JList list = new JList();
+		list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		list.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+			if(e.getValueIsAdjusting()) {
+				//System.out.println(list.getSelectedValue());
+				System.out.println(list.getSelectedValues().length);
+			}
+			}
+		});
+		/*String names[]= new String[10];
+		int index=0;
+		try {
+			FileInputStream fi=new FileInputStream("C:\\Users\\deepsingh44\\Desktop\\abc.txt");
+			BufferedReader br=new BufferedReader(new InputStreamReader(fi));
+			String line=br.readLine();
+			while(line!=null) {
+				names[index++]=line;
+				line=br.readLine();
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		*/
+		
+		
+		//String names[]= {"1","2","3","4"};
+		
+		JScrollPane jp=new JScrollPane(list);
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"A", "B", "C", "D", "E", "F", "G"};
+			
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		jp.setBounds(113, 126, 155, 120);
+		contentPane.add(jp);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("I accept all terms and condition.");
+		
+		chckbxNewCheckBox.setBounds(108, 270, 178, 23);
+		contentPane.add(chckbxNewCheckBox);
+		
+		JButton btnClickMe = new JButton("Click Me");
+		btnClickMe.setEnabled(false);
+		btnClickMe.setBounds(113, 315, 89, 23);
+		contentPane.add(btnClickMe);
 
 		comboBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
@@ -82,6 +145,17 @@ public class ComponentsDemo extends JFrame {
 				}
 			}
 		});
+		
+		chckbxNewCheckBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+			
+				if(chckbxNewCheckBox.isSelected()) {
+					btnClickMe.setEnabled(true);
+				}else {
+					btnClickMe.setEnabled(false);
+				}
+			
+			}
+		});
 	}
-
 }
